3f5e828a1cb1f2cbdd062027dc65ba3fc63ff594
keykat <koin12000@naver.com>
2026-08-15 16:09:26 +0900

    feat: 유저 data 예시 추가

--- packages/data/src/main/java/com/betterafter/template/data/di/RepositoryModule.kt
+++ packages/data/src/main/java/com/betterafter/template/data/di/RepositoryModule.kt
@@ -1,11 +1,20 @@
 package com.betterafter.template.data.di
 
+import com.betterafter.template.data.user.repository.UserRepositoryImpl
+import com.betterafter.template.domain.user.repository.UserRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    // Bind your repositories here
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(
+        impl: UserRepositoryImpl,
+    ): UserRepository
 }
