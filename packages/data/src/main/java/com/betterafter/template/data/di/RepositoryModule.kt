80310b32c337fecf52537b7e98c2e5140a60e4aa
keykat <koin12000@naver.com>
2026-08-15 15:59:42 +0900

    refactor: 불필요한 임포트 제거

--- packages/data/src/main/java/com/betterafter/template/data/di/RepositoryModule.kt
+++ packages/data/src/main/java/com/betterafter/template/data/di/RepositoryModule.kt
@@ -1,12 +1,8 @@
 package com.betterafter.template.data.di
 
-import com.betterafter.template.data.payment.repository.PaymentRepositoryImpl
-import com.betterafter.template.domain.payment.repository.PaymentRepository
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
